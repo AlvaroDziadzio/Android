95340ba439312795a814c156f5adb32bcb0808c9
Alvaro Dziadzio <dziadzio.alvaro@gmail.com>
2019-07-04 17:02:11 -0300

    Initial Trivia Commit

---
+++ Trivia/app/src/main/java/com/alvarodziadzio/trivia/data/Question.kt
@@ -0,0 +1,8 @@
+package com.alvarodziadzio.trivia.data
+
+data class Question(val category: String,
+               val type: String,
+               val difficulty: String,
+               val question: String,
+               val correct_answer: String,
+               val incorrect_answers: List<String>)
---
+++ Trivia/app/src/main/java/com/alvarodziadzio/trivia/data/User.kt
@@ -0,0 +1,5 @@
+package com.alvarodziadzio.trivia.data
+
+data class User(val name: String,
+                val email: String,
+                var points: Int)
